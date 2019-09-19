c3e615cd87df336c0209f4ce8b38083b379f3d9b
Mukhlas Rosyadi <mukhlasrosyadi@outlook.com>
2019-09-19 19:06:41 +0700

    fix

--- simplerandomlsb/modifiedlsb.py
+++ simplerandomlsb/modifiedlsb.py
@@ -14,7 +14,7 @@ def num2bit(x):
 
 
 def convert2txt(bits):
-    flagAndMask = {0x1b:0x60, 0x1c:0x40,0x1d:0x20,0x1e:0x30}
+    flagAndMask = {0x1b:0x60, 0x1c:0x40,0x1d:0x20,0x1e:0x30, 0x1f:0x00}
     mask = 0
     flag = 0
     txt = ''
@@ -24,13 +24,6 @@ def convert2txt(bits):
             flag = i
             mask = flagAndMask[i]
             continue
-        """
-        if flag == 0x1b or flag == 0x1c:
-            txt+=chr(i^mask)
-        if flag == 0x1d:
-            txt+=chr(i^mask)
-        if flag == 0x1e:
-        """
         txt+=chr(i|mask)
     return txt
 
@@ -55,25 +48,26 @@ def get_bits(data):
             if flag != cur_flag:
                 cur_flag = 0x1c
                 txt += num2bit(flag)
-        if ord(i) in range(0x20, 0x30):
+        if ord(i) == 0x20:
             flag = 0x1d
             if flag != cur_flag:
                 cur_flag = 0x1d
                 txt += num2bit(flag)
 
-        if ord(i) in range(0x30, 0x40):
+        if ord(i) in range(0x30, 0x3a):
             flag = 0x1e
             if flag != cur_flag:
                 cur_flag = 0x1e
                 txt += num2bit(flag)
         # convert ke 5-bit terus tambahkan ke txt
-        print i
         txt += num2bit(ord(i) & 0x1f)
     txt += num2bit(0x1f)
     return txt
 
+def get_hex_bits(bits):
+    return [hex(int(bits[i:i+5], 2)) for i in range(0, len(bits), 5)]
 
-def embed(imagearray, data):
+def embed(imagearray, data, seed):
     """
     Random LSB embedding using PRNG
     :param data : data to be embedded
@@ -81,60 +75,44 @@ def embed(imagearray, data):
     :param seed : seed for the random generator
     :return : imagearray contains embedded data
     """
-    height, width = imagearray.shape[:2]  # get the size of the image
-    layer = 1  # greyscale=1, rgb=3, rgba=4
-    arr = np.resize(imagearray, (width*height*layer))  # make the array flat
-    bits = get_bits(data)  # get the bits string
-    i = 0
-    for bit in bits:
-        if bit == '0':
-            arr[i] &= 0b11111110  # embed bit to the lsb
+    rand.seed(seed) #random seed initialization
+    height, width = imagearray.shape[:2] # get the size of the image
+    layer = 1 #greyscale=1, rgb=3, rgba=4
+    arr = np.resize(imagearray,(width*height*layer)) # make the array flat
+    bits = get_bits(data); # get the bits string
+    used = [] # list contains pixels that will have been used
+    for bit in bits: #embed all the bits in to image
+        while True: #find unused pixel
+            r = rand.randint(0, len(arr))
+            if r not in used:
+                used.append(r)
+                break
+        if bit == '0': 
+            arr[r] &= 0b11111110 # embed bit to the lsb
         elif bit == '1':
-            arr[i] |= 0b00000001  # embed bit to the lsb
-        i += 1
-    # resize to the original image size
-    arr = np.resize(arr, (height, width, layer))
+            arr[r] |= 0b00000001 # embed bit to the lsb
+    arr = np.resize(arr,(height,width,layer)) #resize to the original image size
     return arr
 
-
-def extract(imagearray):
+def extract(imagearray, seed, bitlength):
+    rand.seed(seed)
     height, width = imagearray.shape[:2]
-    layer = 1  # greyscale=1, rgb=3, rgba=4
-    arr = np.resize(imagearray, (width*height*layer))
+    layer = 1 #greyscale=1, rgb=3, rgba=4
+    arr = np.resize(imagearray,(width*height*layer))
     bits = ''
-    flag = ''
-    i = 0
-    while True:
-        if (i+1) % 5 == 0:  # periksa 5-bit terkakhir yang dibaca
-            if hex(int(flag, 2)) == 0x1F:  # jika flag = 0x1F keluar dari looping
+    used = []
+    for count in xrange(bitlength):
+        while True:
+            r = rand.randint(0,len(arr))
+            if r not in used:
+                used.append(r)
                 break
-        bits += bin(arr[i])[-1:]  # get the LSB bit and insert into the string
-        flag += bin(arr[i])[-1:]  # untuk pemeriksaan flag
-        i += 1
-    # convert the bits string to 8 bit binary format
-    char_bits = [bits[i:i+5] for i in xrange(0, i, 5)]
-    txt = convert2txt(char_bits)
+        bits += bin(arr[r])[-1:] # get the LSB bit and insert into the string
+    hex_bits = get_hex_bits(bits) 
+    txt = convert2txt(hex_bits) 
     return txt
+              
+text = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890"
+password = "24434"
 
-
-# embedding
-
-#secret = ""
-#image = cv2.imread('lena.png',cv2.IMREAD_UNCHANGED)
-#seed = 24434
-#stegimage = embed(image,secret,seed)
-# cv2.imwrite('steg.png',stegimage)
-
-# extracting
-"""
-seed = 24434
 image = cv2.imread('steg.png',cv2.IMREAD_UNCHANGED)
-bitlength = 15*8 # the embedded string is 15 chars long, 1 char = 8bit
-secret = extract(image,seed,bitlength)
-print "The Secret Message is :",secret
-"""
-bits = get_bits("mantapmantapbang123123!@#&APASIH")
-# convert the bits string to 8 bit binary format
-char_bits = [hex(int(bits[i:i+5], 2)) for i in xrange(0, len(bits), 5)]
-print char_bits 
-print convert2txt(char_bits)
